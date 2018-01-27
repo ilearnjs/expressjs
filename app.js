3f35172d8127e3aad598872be6e33d5a511aa8e9
antonenko.p <antonenko.pvl@gmail.com>
2018-01-27 20:59:53 +0300

    Add simple welocme page

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const blogsRoutes = require('./routes/blogs');
+
+const blogsRoute = require('./routes/blogs');
+const indexRoute = require('./routes/index');
+
+app.set('view engine', 'pug');
+app.set('views', './views')
 
 app.use(express.json());
-
-app.use('/blogs', blogsRoutes);
+app.use('/blogs', blogsRoute);
+app.use('*', indexRoute);
 
 module.exports = app;
